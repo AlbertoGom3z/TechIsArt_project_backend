f0279fadfef519af5e140dc18d89b4f6423decea
Alberto Gomez <01@gom3z.com>
2016-08-01 12:55:44 -0400

    port 80 update

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ mongodb.MongoClient.connect(process.env.MONGODB_URI || url, function (err, datab
 
 console.log("Database connection ready");
 
-var server = app.listen(process.env.PORT || 3000, function () {
+var server = app.listen(process.env.PORT || 80, function () {
 // var server = app.listen(process.env.PORT || 3000, function () {
   var port = server.address().port;// db = database;
     console.log("App now running on port", port);
